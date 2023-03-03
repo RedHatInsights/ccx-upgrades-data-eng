115144015d050a9dfff998f429c5769620492339
Jiří Papoušek <jipapous@redhat.com>
2023-03-03 14:27:04 +0100

    Make RHOBS query more specific

--- ccx_upgrades_data_eng/rhobs_queries.py
+++ ccx_upgrades_data_eng/rhobs_queries.py
@@ -10,13 +10,15 @@ def single_cluster_alerts_and_focs(cluster_id: str) -> str:
 
 def alerts_and_focs(cluster_ids: List[str]) -> str:
     """Return a query for retrieving alerts and focs for serveral clusters."""
-    queries = list()
+    queries = []
 
     for cluster_id in cluster_ids:
         queries.append(
-            f"""alerts{{_id="{cluster_id}"}}
+            f"""alerts{{_id="{cluster_id}", namespace=~"openshift-.*", severity=~"warning|critical"}}
 or
-cluster_operator_conditions{{_id="{cluster_id}"}}"""
+cluster_operator_conditions{{_id="{cluster_id}", condition="Available"}} == 0
+or
+cluster_operator_conditions{{_id="{cluster_id}", condition="Degraded"}} == 1"""
         )
 
     return "\nor\n".join(queries)
