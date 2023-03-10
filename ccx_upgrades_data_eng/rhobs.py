5bc54f937db5fcf919f5fc0494caf4572e09a46d
Juan Diaz Suarez <jdiazsua@redhat.com>
2023-03-10 13:10:40 +0100

    support insecure SSL transport in order to work with mocked SSO servers

--- ccx_upgrades_data_eng/rhobs.py
+++ ccx_upgrades_data_eng/rhobs.py
@@ -47,6 +47,7 @@ def perform_rhobs_request(cluster_id: UUID) -> UpgradeRisksPredictors:
         f"{settings.rhobs_url}{rhobs_endpoint}",
         params={"query": query},
         timeout=settings.rhobs_request_timeout,
+        verify=not settings.mock_sso,
     )
 
     if response.status_code == 404:
