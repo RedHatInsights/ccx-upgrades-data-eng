b9634b7a5dc9fe3a74fc57d4b2b9c5b6d350dbab
Juan Diaz Suarez <jdiazsua@redhat.com>
2023-07-18 16:40:25 +0200

    add support for multi-cluster queries

--- ccx_upgrades_data_eng/tests/test_rhobs.py
+++ ccx_upgrades_data_eng/tests/test_rhobs.py
@@ -6,47 +6,21 @@ from unittest.mock import MagicMock, patch
 import pytest
 from fastapi import HTTPException
 
-from ccx_upgrades_data_eng.rhobs import (
-    alerts_and_focs,
-    single_cluster_alerts_and_focs,
-    perform_rhobs_request,
-)
+from ccx_upgrades_data_eng.rhobs import alerts_and_focs, perform_rhobs_request
 from ccx_upgrades_data_eng.tests import needed_env, RHOBS_EMPTY_REPONSE, RHOBS_RESPONSE
 
 
-def test_single_cluster_alerts_and_focs():
-    """Test if single_cluster_alerts_and_focs returns the expected query."""
-    assert (
-        single_cluster_alerts_and_focs("test")
-        == """console_url{_id="test"}
-or
-alerts{_id="test", namespace=~"openshift-.*", severity=~"warning|critical"}
-or
-cluster_operator_conditions{_id="test", condition="Available"} == 0
-or
-cluster_operator_conditions{_id="test", condition="Degraded"} == 1"""
-    )
-
-
-def test_multi_cluster_alerts_and_focs():
+def test_alerts_and_focs():
     """Test if alerts_and_focs returns the expected query."""
     assert (
         alerts_and_focs(["test1", "test2"])
-        == """console_url{_id="test1"}
+        == """console_url{_id=~"test1|test2"}
 or
-alerts{_id="test1", namespace=~"openshift-.*", severity=~"warning|critical"}
+alerts{_id=~"test1|test2", namespace=~"openshift-.*", severity=~"warning|critical"}
 or
-cluster_operator_conditions{_id="test1", condition="Available"} == 0
+cluster_operator_conditions{_id=~"test1|test2", condition="Available"} == 0
 or
-cluster_operator_conditions{_id="test1", condition="Degraded"} == 1
-or
-console_url{_id="test2"}
-or
-alerts{_id="test2", namespace=~"openshift-.*", severity=~"warning|critical"}
-or
-cluster_operator_conditions{_id="test2", condition="Available"} == 0
-or
-cluster_operator_conditions{_id="test2", condition="Degraded"} == 1"""
+cluster_operator_conditions{_id=~"test1|test2", condition="Degraded"} == 1"""
     )
 
 
